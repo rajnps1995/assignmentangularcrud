8d1f6bc2c2713feec9ee540529e46ae00bbdb6df
Animesh Raj <animesh.raj@neosoftmail.com>
2022-04-04 12:55:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EdaddemployeeComponent } from './components/edaddemployee/edaddemployee.component';
+import { EditemployeeComponent } from './components/editemployee/editemployee.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+{path:'employee',component:EmployeeComponent},
+{path:'add_employee',component:EdaddemployeeComponent},
+{path:'editemployee/:id',component:EditemployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
